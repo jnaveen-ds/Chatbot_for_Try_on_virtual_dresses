ef7c5d212217bc1308275319c1f4edd611749e48
jnaveen-ds <jnaveen.ds@gmail.com>
2024-10-24 14:07:21 +0530

    changes made to app.py

--- app.py
+++ app.py
@@ -53,5 +53,5 @@ def send_whatsapp():
     else:
         return render_template('chatbot.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+	app.run(host="127.0.0.1", debug=True, threaded=False)
